4e59cf81edbf8960045baa8bb7d3fdd43135e60d
Dale Carter <xthedalex@gmail.com>
2022-09-29 01:37:47 -0400

    BugFix splitAndZip Method on Storage Collection Return storage collections the same shape as expected with empty collections when the original contains less than two items.

--- app/TT/Storage.php
+++ app/TT/Storage.php
@@ -29,6 +29,23 @@ class Storage extends Collection
 
     public function splitAndZip(): self
     {
+        // When there are 0 items, return an instance of empty collections the same shape as expected.
+        if ($this->count() == 0) {
+            return new Storage([
+                new Storage(),
+                new Storage()
+            ]);
+        }
+
+        // When there is one item, put this collection in the first index and an empty one in the second.
+        if ($this->count() == 1) {
+            return new Storage([
+                $this,
+                new Storage()
+            ]);
+        }
+
+        // Normal behavior with two or more items.
         $split = $this->splitIn(2);
         return $split[0]->zip($split[1]);
     }

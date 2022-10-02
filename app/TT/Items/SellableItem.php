9611b3e59a929ad7a22bbaed285d1d250b0e392d
Dale Carter <xthedalex@gmail.com>
2022-10-02 18:35:54 -0400

    Add more game data to SellableItem

--- app/TT/Items/SellableItem.php
+++ app/TT/Items/SellableItem.php
@@ -15,16 +15,84 @@ class SellableItem extends Item
             'each' => 10000,
             'location' => 'Davis Mega Mall'
         ],
+
+        'military_titanium' => [
+            'each' => 75000,
+            'location' => 'Military Workshop'
+        ],
+
         'scrap_plastic' => [
             'each' => 3600,
             'location' => 'McKenzie Export'
         ],
+        'refined_flint'=> [
+            'each' => 4250,
+            'location' => 'McKenzie Export'
+        ],
+
         'scrap_emerald' => [
             'each' => 25000,
             'location' => 'Jewelry Store'
         ],
+        'refined_gold' => [
+            'each' => 31000,
+            'location' => 'Jewelry Store'
+        ],
+        'crafted_jewelry' => [
+            'each' => 95000,
+            'location' => 'Jewelry Store'
+        ],
+
+        'tcargodust' => [
+            'each' => 1800,
+            'location' => 'LS Port'
+        ],
+        'crafted_ceramictiles' => [
+            'each' => 40000,
+            'location' => 'LS Port'
+        ],
+        'crafted_computer' => [
+            'each' => 210000,
+            'location' => 'LS Port'
+        ],
+        'scrap_acid' => [
+            'each' => 12800,
+            'location' => 'LS Port'
+        ],
+        'refined_aluminum' => [
+            'each' => 8400,
+            'location' => 'LS Port'
+        ],
+        'crafted_batteries' => [
+            'each' => 150000,
+            'location' => 'LS Port'
+        ],
         'refined_copper' => [
             'each' => 10000,
+            'location' => 'LS Port'
+        ],
+        'refined_planks' => [
+            'each' => 14400,
+            'location' => 'LS Port'
+        ],
+        'refined_bronze' => [
+            'each' => 32000,
+            'location' => 'LS Port'
+        ],
+        'refined_glass' => [
+            'each' => 10000,
+            'location' => 'LS Port'
+        ],
+        'mechanicals_rubber' => [
+            'each' => 10000,
+            'location' => 'LS Port'
+        ],
+        'refined_tin' => [
+            'each' => 8400,
+            'location' => 'LS Port'
+        ],
+        'mechanicals_vehicle_framework' => [
+            'each' => 350000,
             'location' => 'LS Port'
         ],
         'refined_zinc' => [
